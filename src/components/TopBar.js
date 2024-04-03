f1829b233e5bf22dde7c9236a2e6d9509d23c01c
kushSharma924 <kushsharma924@gmail.com>
2024-04-02 20:34:29 -0500

    logo on top now functions as button to reload page

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -4,10 +4,14 @@ import logo from './images/logo.jpg';
 const TopBar = () => {
     const title = 'Flavor Finder';
 
+    function refreshPage() {
+        window.location.reload(false);
+    }
+
     return (
         <div className="top-bar">
             <div className="image">
-                <img src = {logo} width = {125} height = {125} alt = "logo" /> 
+            <button><img src = {logo} width = {115} height = {115} alt = "logo" onClick={refreshPage}/></button>
             </div>
             <h1>{ title }</h1>
         </div>
