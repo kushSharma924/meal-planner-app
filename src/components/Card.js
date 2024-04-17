14db8d202225db5d14a700588e649eb4e30894bf
kushSharma924 <kushsharma924@gmail.com>
2024-04-17 09:58:56 -0500

    seperated ingredients into list

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,11 +2,17 @@ import React from 'react';
 
 const Card = ({ title, imageUrl, ingredients, link }) => {
 
+    const listItems = ingredients.map((ingredient) =>
+    <li key={ingredient}>
+      {ingredient}
+    </li>
+  );
+
     return (
         <div className="card" style={{backgroundImage: `url(${imageUrl.url})`}}>
             <div className="card-content">
                 <h3>{title}</h3>
-                <p>{ingredients.join(', ')}</p>
+                <ul>{listItems}</ul>
                 <a href={link} target="_blank" rel="noopener noreferrer">View Recipe</a>
             </div>
         </div>
